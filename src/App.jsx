fb88e08c044f0a7ccb43118a97a18c2216ea15ba
ayushi tiwari <ayushitiwari565@gmail.com>
2025-03-04 18:03:17 +0530

    abc

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Todo from './Project/Todo/Todo'
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <BrowserRouter basename="/">
       <Todo/>
+    </BrowserRouter>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,20 +6,13 @@ import './index.css'
 import App from './App.jsx'
 
 // createRoot(document.getElementById('root')).render(
-//   // <StrictMode>
-//   //   <App />
-//   // </StrictMode>,
-  
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//       <App />
-//   </React.StrictMode>
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
 // )
-// )
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
       <App />
-    </HashRouter>
   </React.StrictMode>
 );

fe00e149df4190ec49c3352dc907640fa4b90912
ayushi tiwari <ayushitiwari565@gmail.com>
2025-03-04 18:20:08 +0530

    abc

---
+++ main.jsx
@@ -0,0 +1,18 @@
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css'
+import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>
+);

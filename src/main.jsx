f6e113f0562bcdefc1ee2ca34d60a3529b2134d7
ayushi tiwari <ayushitiwari565@gmail.com>
2025-03-04 17:05:58 +0530

    error solve

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   // <StrictMode>
+//   //   <App />
+//   // </StrictMode>,
+  
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//       <App />
+//   </React.StrictMode>
+// )
+// )
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);

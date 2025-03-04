f255d35ab9521a6014756792dbea78802c9e5e79
ayushi tiwari <ayushitiwari565@gmail.com>
2025-03-04 18:17:52 +0530

    abc

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-    <BrowserRouter basename="basename={import.meta.env.BASE_URL}">
+    <BrowserRouter basename="/">
       <Todo/>
     </BrowserRouter>
     </>
